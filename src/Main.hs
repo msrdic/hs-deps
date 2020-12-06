f052131805a15ae3717761ad2ce22e7461993aea
Mladen Srdić <m.s.qubits@gmail.com>
2020-12-06 20:21:28 +0100

    Basic reading of file contents.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,15 @@
 module Main where
 
+import System.IO (Handle, IOMode(ReadMode), withFile)
+import System.IO.Strict (hGetContents)
+
+filePath :: FilePath
+filePath = "./src/Main.hs"
+
+content :: Handle -> IO String
+content = hGetContents
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  c <- withFile filePath ReadMode content
+  print c
